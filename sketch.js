90375a6540e072324d733af2475876cf421ecfb1
JUSTANIDIOTWHOSMILES <93035484+JUSTANIDIOTWHOSMILES@users.noreply.github.com>
2021-10-26 17:37:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var player, playerBase;
+var lostarm,lostarmimage;
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+  lostarmimage = loadImage("./assets/playerArcher.png")
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player);
+  
+  
+
+  lostarm = new PlayerArcher(300,playerBase.position.y - 100,120,120);
+  
+}
+
+function draw() {
+  background(backgroundImg);
+
+  imageMode(CENTER);
+  Engine.update(engine);
+  lostarm.display()
+
+  if(keyIsDown(RIGHT_ARROW)){
+    lostarm.angle =+1;
+  }
+
+  if(keyIsDown(LEFT_ARROW)){
+    lostarm.angle = -1;
+  }
+
+  push();
+rotate(lostarm.angle);
+
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+  image(lostarmimage,lostarm.position.x,lostarm.position.y,200,100);
+  
+console.log("message:" + "my"+ "fucking"+ "computer" +"is" +"hacked" )
+  
+
+  pop();
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+}
+
+
+
+
